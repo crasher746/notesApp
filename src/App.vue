9819dfd2775ea85f6cf05bf90af66760d58eeb77
crasher746 <crasher746@gmail.com>
2020-04-04 22:38:56 +0700

    Add search component

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,28 @@
             <message v-if="message" :message="message"/>
 
             <!--new note-->
-            <newNote :note="note" @addNote="addNote"
+            <newNote :note="note" @addNote="addNote" style="margin-top: 2px"
             />
 
             <!--title-->
-            <div class="note-header">
+            <div class="note-header" style="margin: 36px 0">
                 <h1>{{ title }}</h1>
+                <p></p>
+
+                <!--search-->
+                <search :value="search"
+                        placeholder="Найти заметку"
+                        @search="search = $event"/>
+
+                <!--icons controls-->
                 <div class="icons">
                     <svg :class="{ active: grid }" @click="grid = true" style="cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                     <svg :class="{ active: !grid }" @click="grid = false" style="cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
                 </div>
             </div>
 
-            <!--        note list-->
-            <notes :notes="notes" :grid="grid" @remove="removeNote"
+            <!--note list-->
+            <notes :notes="notesFilter" :grid="grid" @remove="removeNote"
             />
         </div>
       </section>
@@ -33,13 +41,15 @@
 import message from './components/Message'
 import newNote from './components/NewNote'
 import notes from './components/Notes'
+import search from "./components/Search";
 export default {
     components: {
-        message, newNote, notes
+        message, newNote, notes, search
     },
     data () {
         return {
             title: 'Notes App',
+            search: '',
             message: null,
             grid: true,
             note: {
@@ -63,6 +73,23 @@ export default {
                     date: new Date(Date.now()).toLocaleString()
                 }
             ]
+        }
+    },
+    computed: {
+        notesFilter () {
+            let array = this.notes,
+                search = this.search
+            if (!search) return array
+            //Small
+            search = search.trim().toLowerCase()
+            //Filter
+            array = array.filter(function (item) {
+                if (item.title.toLowerCase().indexOf(search) !== -1) {
+                    return item
+                }
+            })
+            //Error
+            return array
         }
     },
     methods: {
